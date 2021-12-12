d55f7cc66288cb91267cfe40d0e3e94fe280baa9
largomst <dingrui.zhen@gmail.com>
2021-12-12 08:43:34 +0800

    reimplement lexer with python

--- hand-parser.py
+++ hand-parser.py
@@ -1,104 +1,92 @@
-import logging
 import sys
 
 EOF = -1
 EOF_TYPE = 1
-NAME =2
+NAME = 2
 COMMA = 3
 LBRACK = 4
 RBRACK = 5
 
-TokensNames = [
-    'n/a','<EOF>','NAME','COMMA','LBRACK','RBRACK'
+TOKEN_NAMES = [
+    'n/a', '<EOF>',"NAME",'COMMAN','LBRACK','RBRACK'
 ]
-
-class AbstractLexer:
-    def get_next_token(self):
-        pass
-    def get_token_name(self, token_type):
-        pass
+class Token:
+    def __init__(self, type_:int, text:str):
+        self.type_ = type_
+        self.text = text
+    
+    def __str__(self):
+        tname = TOKEN_NAMES[self.type_]
+        return f"<{self.text},{tname}>"
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, text:str):
         self.input = text
-        self.pos = 0
-        self.current_char = self.input[self.pos]
-    def consume(self):
-        self.pos += 1
-        if self.pos >= len(self.input) :
-            self.current_char = EOF
-        else:
-            self.current_char = self.input[self.pos]
+        self.p = 0
+        self.c = self.input[self.p]
 
-    def match(self, char):
-        if self.current_char == char:
-            self.consume()
+    def consume(self):
+        self.p += 1
+        if len(self.input) <= self.p:
+            self.c = EOF
         else:
-            raise Exception('expect {} but get {}'.format(char, self.current_char))
-    
-class ListLexer(Lexer, AbstractLexer):
-    def __init__(self, text):
-        super().__init__(text)
+            self.c = self.input[self.p]
+
+
+    def get_token_name(self, type_):
+        return TOKEN_NAMES[type_]
     
     def get_next_token(self):
-        while self.current_char != EOF:
-            if self.current_char in [' ' ,'\n' ,'\t' , '\r']:
+        pass
+class ListLexer(Lexer):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def get_next_token(self):
+        while self.c != EOF:
+            if self.c in ' \n\t\r':
+                # skip ws
                 self.WS()
+                # continue
+            elif self.c == ',':
                 self.consume()
-            elif self.current_char == ',':
+                return Token(COMMA, ",")
+            elif self.c == '[':
                 self.consume()
-                return Token(COMMA, ',')
-            elif self.current_char == '[':
+                return Token(LBRACK, "[")
+            elif self.c == ']':
                 self.consume()
-                return Token(LBRACK, '[')
-            elif self.current_char == ']':
-                self.consume()
-                return Token(RBRACK, ']')
+                return Token(RBRACK, "]")
             else:
                 if self.isLetter():
                     return self.NAME()
                 else:
-                    raise Exception("invalid character: "  + self.current_char + '\n')
-                    os.exit(1)
+                    raise Exception("invalid characer: ", self.c)
         return Token(EOF_TYPE,EOF)
-    
+
+
 
     def isLetter(self):
-        return self.current_char.isalpha()
-    
+        return self.c.isalpha()
+
+    def WS(self):
+        while self.c in ' \t\n\r':
+            self.consume()
+        
     def NAME(self):
         buf = []
         while True:
-            buf.append(self.current_char)
+            buf.append(self.c)
             self.consume()
             if self.isLetter():
                 continue
             else:
                 break
         return Token(NAME, ''.join(buf))
-
-    def WS(self):
-        while self.current_char in [' ' ,'\n' ,'\t' , '\r']:
-            self.consume()       
-    
-    def get_token_name(self, token_type):
-        return TokensNames[token_type]
-
-
-class Token:
-    def __init__(self, type, text):
-        self.type = type
-        self.text = text
-
-    def __str__(self):
-        tname = TokensNames[self.type]
-        return "<" + self.text + "," + tname + ">"
-
-    
-
-if __name__ == "__main__":
+            
+if __name__ == '__main__':
     lexer = ListLexer(sys.argv[1])
     token = lexer.get_next_token()
-    while token.type != EOF_TYPE:
+    while token.type_ != EOF_TYPE:
         print(token)
         token = lexer.get_next_token()
